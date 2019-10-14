ed470da91280c70708617f0c9f1d3e271dfffe28
Jason <jason13000@hotmail.fr>
2019-10-14 23:11:32 +0200

    création de la base

---
+++ include/navbar.php
@@ -0,0 +1,43 @@
+<!--Navbar-->
+<nav class="navbar navbar-expand-lg navbar-light">
+
+  <!-- Navbar brand -->
+  <a class="navbar-brand" href="index.php"><img id="logo" src="img/logo.png" alt="logo"></a>
+
+  <!-- Collapse button -->
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#basicExampleNav"
+    aria-controls="basicExampleNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <!-- Collapsible content -->
+  <div class="collapse navbar-collapse" id="basicExampleNav">
+
+    <!-- Links -->
+    <ul class="navbar-nav mx-auto">
+      <li class="nav-item">
+        <a class="nav-link" href="#">Coffrets</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Shampooings</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Masque cheveux</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Soins cheveux</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Spray</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Huiles</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Cires</a>
+      </li>
+  </div>
+  <!-- Collapsible content -->
+
+</nav>
+<!--/.Navbar-->
---
+++ index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <?php require "include/head.php"; ?>
+    <title>Aïda M'DALLA Cosmétique capillaire | Produits pour cheveux qualité Professionnelle</title>
+</head>
+<body class="p-0 h-100">
+
+<?php require "include/navbar.php"; ?>
+
+<div id="carouselExampleControls" class="carousel slide carousel-fade m-0" data-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img class="d-block w-100" src="img/01.jpg"
+        alt="First slide">
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="img/02.jpg"
+        alt="Second slide">
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="img/03.jpg"
+        alt="Third slide">
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="img/04.jpg"
+        alt="Fourth slide">
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
+
+<?php require "include/footer.php"; ?>
+    
+</body>
+</html>
